800375973ba91904a2b1bb798bf3b61d68110843
Adamtheboss9 <adamtheboss78@gmail.com>
2026-03-23 09:13:44 -0400

    Got Canvas to display. Started figuring out how to do stuff with it.

--- public/js/basketBallGame.js
+++ public/js/basketBallGame.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("basketball");
   const ctx = canvas.getContext("2d");
-
+  ctx.rect(0, 0, 300, 400);
+  ctx.fillStyle = "red";
+    ctx.stroke();
+    ctx.fill();
   const image = new Image();
   image.src = "../img/hoop.png";
 
   image.onload = () => {
-    ctx.drawImage(image, 10, 10, 150, 100);
+    ctx.drawImage(image, 130, 50, 150, 100);
 
-    // ctx.beginPath();
-    // ctx.rect(10, 10, 150, 100);
-    // ctx.stroke();
+    ctx.beginPath();
+    
+   
   };
 });
